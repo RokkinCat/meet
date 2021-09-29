6b2beaf2c5b221c16abf574a0f88167c69defbe5
Nick Gartmann <nick@rokkincat.com>
2021-09-29 11:26:09 -0500

    Change hostname

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :meet, MeetWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "meet.nickgartmann.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- config/runtime.exs
+++ config/runtime.exs
@@ -6,7 +6,7 @@ if config_env() == :prod do
 
   config :meet, MeetWeb.Endpoint,
     server: true,
-    url: [host: "#{app_name}.fly.dev", port: 80],
+    url: [host: "meet.nickgartmann.com", port: 80],
     http: [
       port: String.to_integer(System.get_env("PORT") || "4000"),
       transport_options: [socket_opts: [:inet6]]
